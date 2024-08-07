3f061f9c5af24b7fbbe8dbb1ff5b3efce1767fd3
Om Yadav <omydv3@gmail.com>
2024-08-07 15:43:09 +0530

    connect to Mongo Atlas

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`MongoDB connected Successfully !! DB Host: ${connectionInstance.connection.host}`);
+
+        
+    } catch (error) {
+        console.log(" MONGODB connection FAILED ", error);
+        process.exit(1)
+
+    }
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-import express from 'express';
+import dotenv from "dotenv"
+import connectDB from './db/index.js';
 
+dotenv.config({
+   path:'./env'
+})
+
+import express from "express";
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Server is ready buddy!");
 
-})
+connectDB()
+
+/*
+(async () => {
+   try {
+      await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+      app.on("ERROR: ", (error) => {
+         console.error("ERROR: ", error);
+         throw error;
+      });
+
+      app.listen(process.env.PORT,  () => {
+         console.log(`App is listen on port ${process.env.PORT}`);
+         
+      })
+   } catch (error) {
+      console.error("ERROR: ", error);
+      throw error;
+   }
+})();
+*/
+
+
+
+
+app.get("/", (req, res) => {
+   res.send("Server is ready buddy!");
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server at http://localhost:${port}`)
-})
+   console.log(`Server at http://localhost:${port}`);
+});
