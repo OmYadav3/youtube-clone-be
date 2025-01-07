b61e38f1b5eeae7f346bf9dda7e97dcc7057ce1b
Om Yadav <omydv3@gmail.com>
2025-01-08 04:45:16 +0530

    Add like, tweet, playlist and comment models

---
+++ src/models/comment.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose'
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        video: {
+            type: mongoose.Types.ObjectId,
+            ref: "Video"
+        },
+        owner: {
+            type: mongoose.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+commentSchema.plugin(mongooseAggregatePaginate)
+
+export const Comment = moongoose.model("Comment", commentSchema )
---
+++ src/models/playlist.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose'
+
+const playlistSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        
+        description: {
+            type: String,
+            required: true
+        },
+        videos: [{
+            type: mongoose.Types.ObjectId,
+            ref: "Video"
+        }],
+        owner: {
+          type: mongoose.Types.ObjectId,
+          ref: "User"  
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Playlist = mongoose.mogel("Playlist", playlistSchema)
